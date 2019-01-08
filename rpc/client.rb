f71b874ffdbefe095df96421f2b2df83478b87dd
Zhenyuan Lau <zhenyuanlau@icloud.com>
2019-01-08 10:20:50 +0800

    implement basic rpc

--- rpc/client.rb
+++ rpc/client.rb
@@ -1,7 +1,38 @@
 require 'socket'
 
-client = TCPSocket.new('0.0.0.0', 8888)
+# Client Stub
+class ClientStub
+  def initialize(address)
+    @host, @port = address.split(':')
+    @client = TCPSocket.new(@host, @port)
+  end
 
-client.write("hi")
+  def method_missing(name, *args)
+    data = Marshal.dump([name, args])
+    @client.write(data)
+    @client.close_write
+    data = @client.read
+    message = Marshal.load(data)
+    @client.close
+    message
+  end
+end
 
-client.close
+# Client Request
+class Request
+  def initialize(body)
+    @body = body
+  end
+end
+
+class Client
+  class << self
+    def main
+      stub = ClientStub.new('localhost:8888')
+      message = stub.greet('hello')
+      p "Greeting: #{message}"
+    end
+  end
+end
+
+Client.main
--- rpc/server.rb
+++ rpc/server.rb
@@ -1,10 +1,33 @@
 require 'socket'
 
-server = TCPServer.new(8888)
+class ServerStub
+  class << self
+    def start
+      stub = ServerStub.new(8888)
+      stub.run
+    end
+  end
 
-Socket.accept_loop(server) do |connection|
-  request = connection.read
-  puts request
-  connection.write(request)
-  connection.close
+  def initialize(port)
+    @server = TCPServer.new(port)
+  end
+
+  def run
+    Socket.accept_loop(@server) do |connection|
+      request = connection.read
+      method, args = Marshal.load(request)
+      response = send(method.to_sym, *args)
+      data = Marshal.dump(response)
+      connection.write(data)
+      connection.close
+    end
+  end
+
+  def method_missing(name, *args)
+    'hi'
+  end
+
 end
+
+
+ServerStub.start
